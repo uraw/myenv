2d8825fd66cb253df72f38f6862849bfb510390f
uraw <uraw@urgy.net>
2022-01-18 14:13:01 +0900

    Refactoring

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -3,16 +3,16 @@
 
 VERSION=${1:-4.4}
 SRCARCHIVE=ffmpeg-${VERSION}.tar.xz
-DIRNAME=ffmpeg-${VERSION}
+DIRECTORY=ffmpeg-${VERSION}
 
 rm -rf ffmpeg-${VERSION}*
 
 sudo apt install -y make nasm yasm libx264-dev libx265-dev
 wget https://ffmpeg.org/releases/${SRCARCHIVE}
 tar xf ${SRCARCHIVE}
-cd ${DIRNAME}
+cd ${DIRECTORY}
 ./configure --enable-gpl --enable-libx264 --enable-nonfree
 make -j
 sudo porg -lD make install
 cd ..
-rm -rf ${DIRNAME}
+rm -rf ${DIRECTORY}
